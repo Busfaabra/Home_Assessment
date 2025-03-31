562cd72cb61cef997fcdc4a9b358d3b9c46ec9cf
busfaabra <takniashvili99@gmail.com>
2025-03-31 11:46:37 +0400

    update

--- app/(main)/profile/InfoInput.js
+++ app/(main)/profile/InfoInput.js
@@ -1,14 +1,11 @@
 import { TextInput, View } from 'react-native';
 
-import { useColorScheme } from 'nativewind';
 import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const duration = 150;
 
-export default function InfoInput({ placeholder, value, onChangeText }) {
+export default function InfoInput({ placeholder, value, onChangeText, ...rest }) {
   const holder = useSharedValue(0);
-
-  const { colorScheme } = useColorScheme();
 
   const handleFocus = () => {
     holder.value = withTiming(1, { duration });
@@ -33,7 +30,7 @@ export default function InfoInput({ placeholder, value, onChangeText }) {
 
   return (
     <View className="bg-gray-100 h-14 rounded-xl justify-center mb-3.5">
-      <TextInput className="h-full pl-4 text-sm" selectionColor="#111111" value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} />
+      <TextInput className="h-full pl-4 text-sm" selectionColor="#111111" value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} {...rest} />
       <Animated.Text style={holderAnim} className="absolute left-4 text-[#03071280] pointer-events-none">
         {placeholder}
       </Animated.Text>
