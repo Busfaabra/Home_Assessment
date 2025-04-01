7672d9c464cc9a460dd258845f76b06b5faff9f1
busfaabra <takniashvili99@gmail.com>
2025-04-01 19:39:16 +0400

    update

--- components/profile/InfoInput.js
+++ components/profile/InfoInput.js
@@ -8,6 +8,8 @@ const duration = 150;
 export default function InfoInput({ placeholder, value, onChangeText, ...rest }) {
   const holder = useSharedValue(0);
 
+  const x = placeholder === 'Position' ? 4.1 : 5;
+
   const handleFocus = () => {
     holder.value = withTiming(1, { duration });
   };
@@ -20,13 +22,13 @@ export default function InfoInput({ placeholder, value, onChangeText, ...rest })
     return {
       transform: [
         {
-          scale: interpolate(holder.value, [0, 1], [1, 0.83]),
+          scale: interpolate(holder.value, [0, 1], [1, 0.85]),
         },
         {
-          translateY: interpolate(holder.value, [0, 1], [0, -10]),
+          translateY: interpolate(holder.value, [0, 1], [0, -9]),
         },
         {
-          translateX: interpolate(holder.value, [0, 1], [0, -6]),
+          translateX: interpolate(holder.value, [0, 1], [0, -x]),
         },
       ],
     };
@@ -34,7 +36,7 @@ export default function InfoInput({ placeholder, value, onChangeText, ...rest })
 
   return (
     <View className="bg-primary h-[56] justify-center rounded-xl mb-[14]">
-      <TextInput className="h-full pl-[16] pt-[16]" selectionColor={Colors.STextColor} value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} {...rest} />
+      <TextInput className="h-full pl-[16] pt-[17]" selectionColor={Colors.STextColor} value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} {...rest} />
       <Animated.Text style={holderAnim} className="absolute left-[16] text-STextColor pointer-events-none">
         {placeholder}
       </Animated.Text>
