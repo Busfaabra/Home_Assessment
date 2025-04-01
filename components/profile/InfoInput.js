db88f7fe18ba8fad7dbecc9ec5d73dfd253d3506
busfaabra <takniashvili99@gmail.com>
2025-04-01 19:30:47 +0400

    update

--- components/profile/InfoInput.js
+++ components/profile/InfoInput.js
@@ -26,7 +26,7 @@ export default function InfoInput({ placeholder, value, onChangeText, ...rest })
           translateY: interpolate(holder.value, [0, 1], [0, -10]),
         },
         {
-          translateX: interpolate(holder.value, [0, 1], [0, -7]),
+          translateX: interpolate(holder.value, [0, 1], [0, -6]),
         },
       ],
     };
@@ -34,7 +34,7 @@ export default function InfoInput({ placeholder, value, onChangeText, ...rest })
 
   return (
     <View className="bg-primary h-[56] justify-center rounded-xl mb-[14]">
-      <TextInput className="h-full pl-[16] pt-[10] text-sm" selectionColor={Colors.STextColor} value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} {...rest} />
+      <TextInput className="h-full pl-[16] pt-[16]" selectionColor={Colors.STextColor} value={value} onChangeText={onChangeText} onFocus={handleFocus} onBlur={handleBlur} {...rest} />
       <Animated.Text style={holderAnim} className="absolute left-[16] text-STextColor pointer-events-none">
         {placeholder}
       </Animated.Text>
