7b6b949b3df20582634cfbed9edf9cc4258e28a1
busfaabra <takniashvili99@gmail.com>
2025-04-02 14:26:26 +0400

    fix: selectableButton border animation

--- components/profile/SelectableButton.js
+++ components/profile/SelectableButton.js
@@ -1,19 +1,20 @@
 import { Image, Pressable, StyleSheet, Text } from 'react-native';
-import Animated, { interpolate, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import Animated, { interpolateColor, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import Colors from '../constants/colors';
 
 const AnimButton = Animated.createAnimatedComponent(Pressable);
 
 export default function SelectableButton({ homeSheetRef, selectedSex, progresSex }) {
   const sexStyle = useAnimatedStyle(() => {
     return {
-      borderColor: progresSex.value ? '#085F4C' : '#fff',
-      borderWidth: interpolate(progresSex.value, [0, 1], [0, 1]),
+      borderColor: interpolateColor(progresSex.value, [1, 0], [Colors.green, Colors.white]),
+      borderWidth: 1,
     };
   });
   return (
     <AnimButton
       style={sexStyle}
-      className="flex-row items-center justify-between bg-gray-100 h-[48] rounded-xl "
+      className="flex-row items-center justify-between bg-primary h-[48] rounded-xl"
       onPress={() => {
         homeSheetRef.current.show();
         progresSex.value = withTiming(1);
