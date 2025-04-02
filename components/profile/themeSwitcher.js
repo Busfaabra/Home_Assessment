a52b206c87086f2535f6085f80dc1f2c1fcf2356
busfaabra <takniashvili99@gmail.com>
2025-04-02 17:41:03 +0400

    update

--- components/profile/themeSwitcher.js
+++ components/profile/themeSwitcher.js
@@ -1,6 +1,7 @@
 import { View, Pressable, StyleSheet } from 'react-native';
 import useColorScheme from '../../hooks/useColorScheme';
 import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const T_WIDTH = 100;
 const PADDING = 2.8;
@@ -9,6 +10,14 @@ const C_WIDTH = 34;
 
 const LEFTOVER_SPACE = T_WIDTH - (C_WIDTH + MARGIN_LEFT + PADDING);
 const TRANSLATE_X = LEFTOVER_SPACE - (MARGIN_LEFT + PADDING);
+
+function Icon({ name, size, className }) {
+  return (
+    <View className={className}>
+      <MaterialIcons name={name} size={size} color="white" />
+    </View>
+  );
+}
 
 export default function ThemeSwitcher() {
   const { colorScheme, setColorScheme } = useColorScheme();
@@ -37,6 +46,8 @@ export default function ThemeSwitcher() {
     <Pressable onPress={onToggle} style={styles.press} className="mt-36 bg-black dark:bg-slate-800 rounded-full">
       <View className="bg-gray-700 rounded-full justify-center w-full h-full">
         <View style={{ borderWidth: 2 }} className="absolute top-0.5 left-0.5 right-0.5 bottom-0.5 border-gray-500 rounded-full" />
+        <Icon className="absolute left-2.5" name="dark-mode" size={30} />
+        <Icon className="absolute right-3 " name="light-mode" size={26} />
         <Animated.View style={animToggle} className="bg-gray-500 rounded-full " />
       </View>
     </Pressable>
